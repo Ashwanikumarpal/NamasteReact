0b0dd279f9099f477ec85d8849b3ae96c93e0525
unknown <ashwanikumarpal0005@gmail.com>
2024-10-07 20:05:58 +0530

    Episode1

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", id: "ac" },
+  "hello World from the Rant"
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
